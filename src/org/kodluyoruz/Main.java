8b17a0375be4ca30bc038e53d020cf7bdfa5bc0e
mertkeskin <keskinmert@itu.edu.tr>
2021-01-05 20:14:28 +0300

    İlk push...

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -3,6 +3,56 @@ package org.kodluyoruz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Queue<String> q1 = new Queue<String>();
+
+        System.out.println(q1.peek());
+
+        q1.add("Ali");
+        q1.add("Veli");
+        q1.add("49");
+        q1.add("50");
+
+        System.out.println(q1.peek());
+        System.out.println(q1.peek());
+
+        q1.add("ekleme");
+
+        try {
+            System.out.println(q1.poll());
+            System.out.println(q1.poll());
+            System.out.println(q1.poll());
+            System.out.println(q1.poll());
+            System.out.println(q1.poll());
+            System.out.println(q1.poll());
+
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Queue<Integer> q2 = new Queue<Integer>();
+
+        System.out.println(q2.peek());
+
+        q2.add(47);
+        q2.add(48);
+        q2.add(49);
+        q2.add(50);
+
+        System.out.println(q2.peek());
+        System.out.println(q2.peek());
+
+        q2.add(51);
+
+        try {
+            System.out.println(q2.poll());
+            System.out.println(q2.poll());
+            System.out.println(q2.poll());
+            System.out.println(q2.poll());
+            System.out.println(q2.poll());
+            System.out.println(q2.poll());
+
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
